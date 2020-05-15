ab2b78fe38cdd1be87c29c54f76e9e8f28376ad4
Antonio Hernandez <antonio@mlmusings.com>
2020-05-15 13:58:20 -0500

    Commited most of the code.

---
+++ lambda_function.py
@@ -0,0 +1,47 @@
+import json
+import os
+import glob
+import boto3
+import subprocess
+from datetime import datetime
+
+repository_name = 'esaleatorio'
+commit_message = ''.join(['Pushed new posts, deleted old ones from AWS Lambda at ', str(datetime.now())])
+folder_path = 'content/'
+test_file = "Ramdom text from AWS Lamda, Test 2!"
+branch_name = 'master'
+
+
+# Create new posts in /tmp directory of the aws lambda environment
+subprocess.call('news_handler.py', shell=True)
+    
+def lambda_handler(event, context):
+    client = boto3.client('codecommit')
+    response = client.get_folder(repositoryName=repository_name, folderPath=folder_path)
+    keys = response['files']
+    commit_id = response['commitId']
+        
+    old_posts = []
+    for key in keys:
+        old_posts.append({'filePath': key.get('absolutePath')})
+
+    posts = glob.glob('/tmp/post*.md')
+
+    new_posts = []
+    for post in posts:
+        new_posts.append({'filePath': ''.join(['content/posts/', post]),
+                        #'fileContent': str.encode(post), 
+                        'sourceFile': {'filePath': ''.join(['/tmp/', post])}
+                        })
+        
+    commit_response = client.create_commit(
+        repositoryName=repository_name,
+        branchName=branch_name,
+        parentCommitId=commit_id,
+        authorName='Antonio Hernandez',
+        email='antonio@mlmusings.com',
+        commitMessage=commit_message,
+        keepEmptyFolders=True,
+        putFiles=new_posts,
+        deleteFiles = old_posts
+        )
